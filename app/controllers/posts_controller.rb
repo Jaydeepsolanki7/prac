f49eff1c3c6051170393fd29d875095749616c0d
Jaydeepsolanki76 <jay.solanki7694@gmail.com>
2023-12-13 18:25:58 +0530

    serializer

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,47 @@
 class PostsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     @posts = Post.all
     render json: @posts, each_serializer: PostSerializer
   end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+    if @post.present?
+      render json: @post, serializer: PostSerializer
+    else
+      render json: { error: "Post not found" }
+    end
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    debugger
+    @post = Post.new(post_params)
+    if @post.save
+      render json: @post
+    else
+      render json: {error: "Post can't be created"}
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    if @post.present?
+      @post.destroy
+      render json: { message: "Post succesfully deleted."}, status: 200
+    else
+      render json: { error: "Post not found" }
+    end
+  end
+
+  private
+
+    def post_params
+      params.permit(:title, :description)
+    end
 end
--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,3 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :body
+  attributes:body
 end
